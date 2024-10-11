79651eb0f9eb24cfa19d5c1286a466261f29c000
Christophe Fontaine <cfontain@redhat.com>
2024-10-11 09:46:48 +0000

    infra: add packet tracing framework
    Introduce a way to attach per-node packet tracing to mbufs. Nodes can
    append a new trace item using gr_mbuf_trace_add(). Trace items contain
    the node id, processing CPU id, a time stamp and an opaque buffer to
    store tracing data.
    
    The buffer returned by gr_mbuf_trace_add() should contain stateless data
    (e.g. no pointers) to avoid keeping references on objects that may
    become invalid in the future.
    
    Allocate trace items from a mempool and link them together using a tail
    queue. Store the head of the tail queue in the mbuf private data. To
    avoid any overlap with existing private data, shift them after the tail
    queue head and update functions to access private data accordingly.
    
    Source nodes must check if they should begin tracing a particular mbuf
    by checking if the GR_IFACE_F_PACKET_TRACE flag is set on the receiving
    interface. Other nodes can then rely on gr_mbuf_is_traced() to determine
    if a given mbuf already contains trace items from previous nodes and
    add their own contribution to the list.
    
    Sink nodes should add their trace items and detach them from the mbuf
    with gr_mbuf_trace_finish() before the mbuf is freed. This function will
    enqueue the trace item tail queue into a ring waiting for control plane
    to flush it.
    
    If no items are available or if the ring is full, gr_mbuf_trace_add()
    and gr_mbuf_trace_finish() will dequeue oldest traces from the ring to
    make more available.
    
    Here is a simplified representation of the trace ring after successive
    calls of gr_mbuf_trace_finish(). Packets in parentheses are only there
    for context. Mbufs are not stored in the ring.
    
      -------------------------------------------------------------------
      head                                            tail
      -------------------------------------------------------------------
      (packet 1)      (packet 2)      (packet 3)      (packet 4)
    
      trace node1     trace node1     trace node1     trace node1
           |               |                               |
           v               v                               v
      trace node2     trace node2                     trace node2
           |               |                               |
           v               v                               v
      trace node3     trace node6                     trace node3
           |                                               |
           v                                               v
      trace node4                                     trace node4
    
    Control plane can flush the trace ring and format each item into a byte
    buffer using gr_trace_dump(). Every traced packet is prefixed with the
    formatted time stamp and the received CPU id. Then, each trace item is
    printed, starting with the node name. And, based on the node_id, each
    trace item is passed to the corresponding node trace_format callback (if
    any). Here is an example output:
    
    --------- 09:55:44.596412836 cpu 1 ---------
    port_rx: port 0 queue 0
    eth_input: d2:ad:ca:ca:a4:11 > 33:33:ff:00:00:01 type=IPv6(0x86dd) iface=ge2ten1
    ip6_input: fd00:ba4:1::2 > ff02::1:ff00:1 ttl=255 proto=ICMPv6(58)
    ip6_input_local:
    icmp6_input: neigh solicit who has fd00:ba4:1::1? / Option src_lladdr=d2:ad:ca:ca:a4:11
    ndp_ns_input: neigh advert fd00:ba4:1::1 is at / Option target_lladdr=d2:f0:0c:ba:a4:11
    ip6_output: fd00:ba4:1::1 > fd00:ba4:1::2 ttl=255 proto=ICMPv6(58)
    eth_output: d2:f0:0c:ba:a4:11 > d2:ad:ca:ca:a4:11 type=IPv6(0x86dd) iface=ge2ten1
    port_tx: port 0 queue 0
    
    This commit only contains the infrastructure code. Control plane API,
    CLI commands and each node tracing implementation will be done in
    separate patches.
    
    Signed-off-by: Christophe Fontaine <cfontain@redhat.com>
    Co-authored-by: Robin Jarry <robin@jarry.cc>

--- modules/infra/datapath/gr_trace.h
+++ modules/infra/datapath/gr_trace.h
@@ -5,9 +5,23 @@
 #ifndef _GR_INFRA_PACKET_TRACE
 #define _GR_INFRA_PACKET_TRACE
 
+#include <rte_graph.h>
 #include <rte_mbuf.h>
+
+#include <sys/queue.h>
 
 // Write a log message with detailed packet information.
 void trace_log_packet(const struct rte_mbuf *m, const char *node, const char *iface);
+
+// Callback associated with each node that will be invoked by gr_trace_dump
+// to format each individual trace items.
+typedef int (*gr_trace_format_cb_t)(char *buf, size_t buf_len, const void *data, uint16_t data_len);
+
+// Format the buffered trace items and empty the buffer.
+// Return the number of bytes written to buffer or a negative value on error.
+int gr_trace_dump(char *buf, size_t buf_len);
+
+// Empty the trace buffer.
+void gr_trace_clear(void);
 
 #endif
