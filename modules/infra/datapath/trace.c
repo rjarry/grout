rjarry/grout

commit cf84f676455d5382dbe2c09f8019451d35544073

trace: add ipv6 support
Add ICMPv6 header definitions. Dissect and print IPv6 packets when
tracing is enabled.

Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/infra/datapath/trace.c
@@ -2,6 +2,7 @@
 // Copyright (c) 2024 Robin Jarry
 
 #include "gr_datapath.h"
+#include "gr_icmp6.h"
 
 #include <gr_log.h>
 #include <gr_net_types.h>
@@ -13,7 +14,16 @@
 #include <rte_graph_worker.h>
 #include <rte_icmp.h>
 #include <rte_ip.h>
+#include <rte_ip6.h>
 #include <rte_mbuf.h>
+
+static ssize_t trace_icmp6(
+	char *buf,
+	const size_t len,
+	const struct rte_mbuf *,
+	size_t *offset,
+	uint16_t payload_len
+);
 
 void trace_packet(const char *node, const char *iface, const struct rte_mbuf *m) {
 	char buf[BUFSIZ], src[64], dst[64];
@@ -101,6 +111,58 @@ ipv4:
 
 		break;
 	}
+	case RTE_ETHER_TYPE_IPV6: {
+		const struct rte_ipv6_hdr *ip6;
+		uint16_t payload_len;
+		int proto;
+
+		ip6 = rte_pktmbuf_mtod_offset(m, const struct rte_ipv6_hdr *, offset);
+		offset += sizeof(*ip6);
+		inet_ntop(AF_INET6, &ip6->src_addr, src, sizeof(src));
+		inet_ntop(AF_INET6, &ip6->dst_addr, dst, sizeof(dst));
+		n += snprintf(
+			buf + n,
+			sizeof(buf) - n,
+			" / IPv6 %s > %s ttl=%hhu",
+			src,
+			dst,
+			ip6->hop_limits
+		);
+		payload_len = rte_be_to_cpu_16(ip6->payload_len);
+		proto = ip6->proto;
+
+		for (;;) {
+			size_t ext_size = 0;
+			int next_proto = rte_ipv6_get_next_ext(
+				rte_pktmbuf_mtod_offset(m, const uint8_t *, offset),
+				proto,
+				&ext_size
+			);
+			if (next_proto < 0)
+				break;
+			if (proto != IPPROTO_HOPOPTS)
+				n += snprintf(
+					buf + n,
+					sizeof(buf) - n,
+					" Ext(%hhu len=%zu)",
+					proto,
+					ext_size
+				);
+			offset += ext_size;
+			proto = next_proto;
+		};
+
+		switch (proto) {
+		case IPPROTO_ICMPV6:
+			n += trace_icmp6(buf + n, sizeof(buf) - n, m, &offset, payload_len);
+			break;
+		default:
+			n += snprintf(buf + n, sizeof(buf) - n, " nh=%hhu", proto);
+			break;
+		}
+
+		break;
+	}
 	case RTE_ETHER_TYPE_ARP: {
 		const struct rte_arp_hdr *arp;
 
@@ -146,4 +208,127 @@ ipv4:
 	n += snprintf(buf + n, sizeof(buf) - n, ", (pkt_len=%u)", m->pkt_len);
 
 	LOG(NOTICE, "[%s %s] %s", node, iface, buf);
+}
+
+static ssize_t trace_icmp6(
+	char *buf,
+	const size_t len,
+	const struct rte_mbuf *m,
+	size_t *offset,
+	uint16_t payload_len
+) {
+	const struct icmp6 *icmp6;
+	const struct icmp6_opt *opt = NULL;
+	char dst[INET6_ADDRSTRLEN];
+	ssize_t n = 0;
+
+	n += snprintf(buf + n, len - n, " / ICMPv6");
+	icmp6 = rte_pktmbuf_mtod_offset(m, const struct icmp6 *, *offset);
+	*offset += sizeof(*icmp6);
+	payload_len -= sizeof(*icmp6);
+
+	switch (icmp6->type) {
+	case ICMP6_TYPE_ECHO_REQUEST: {
+		const struct icmp6_echo_request *req = PAYLOAD(icmp6);
+		*offset += sizeof(*req);
+		payload_len -= sizeof(*req);
+		n += snprintf(
+			buf + n,
+			len - n,
+			" echo request id=%u seq=%u",
+			rte_be_to_cpu_16(req->ident),
+			rte_be_to_cpu_16(req->seqnum)
+		);
+		break;
+	}
+	case ICMP6_TYPE_ECHO_REPLY: {
+		const struct icmp6_echo_reply *reply = PAYLOAD(icmp6);
+		*offset += sizeof(*reply);
+		payload_len -= sizeof(*reply);
+		n += snprintf(
+			buf + n,
+			len - n,
+			" echo reply id=%u seq=%u",
+			rte_be_to_cpu_16(reply->ident),
+			rte_be_to_cpu_16(reply->seqnum)
+		);
+		break;
+	}
+	case ICMP6_TYPE_ROUTER_SOLICIT:
+		const struct icmp6_router_solicit *rs = PAYLOAD(icmp6);
+		*offset += sizeof(*rs);
+		payload_len -= sizeof(*rs);
+		n += snprintf(buf + n, len - n, " router solicit");
+		opt = PAYLOAD(rs);
+		break;
+	case ICMP6_TYPE_ROUTER_ADVERT:
+		const struct icmp6_router_advert *ra = PAYLOAD(icmp6);
+		*offset += sizeof(*ra);
+		payload_len -= sizeof(*ra);
+		n += snprintf(buf + n, len - n, " router advert");
+		opt = PAYLOAD(ra);
+		break;
+	case ICMP6_TYPE_NEIGH_SOLICIT: {
+		const struct icmp6_neigh_solicit *ns = PAYLOAD(icmp6);
+		*offset += sizeof(*ns);
+		payload_len -= sizeof(*ns);
+		inet_ntop(AF_INET6, &ns->target, dst, sizeof(dst));
+		n += snprintf(buf + n, len - n, " neigh solicit who has %s?", dst);
+		opt = PAYLOAD(ns);
+		break;
+	}
+	case ICMP6_TYPE_NEIGH_ADVERT: {
+		const struct icmp6_neigh_advert *na = PAYLOAD(icmp6);
+		*offset += sizeof(*na);
+		payload_len -= sizeof(*na);
+		inet_ntop(AF_INET6, &na->target, dst, sizeof(dst));
+		n += snprintf(buf + n, len - n, " neigh advert %s is at", dst);
+		opt = PAYLOAD(na);
+		break;
+	}
+	default:
+		n += snprintf(buf + n, len - n, " type=%hhu code=%hhu", icmp6->type, icmp6->code);
+		payload_len = 0;
+		break;
+	}
+
+	while (payload_len >= 8 && opt != NULL) {
+		switch (opt->type) {
+		case ICMP6_OPT_SRC_LLADDR: {
+			const struct icmp6_opt_lladdr *ll = PAYLOAD(opt);
+			n += snprintf(
+				buf + n,
+				len - n,
+				" / Option src_lladdr=" ETH_ADDR_FMT,
+				ETH_ADDR_SPLIT(&ll->mac)
+			);
+			break;
+		}
+		case ICMP6_OPT_TARGET_LLADDR: {
+			const struct icmp6_opt_lladdr *ll = PAYLOAD(opt);
+			n += snprintf(
+				buf + n,
+				len - n,
+				" / Option target_lladdr=" ETH_ADDR_FMT,
+				ETH_ADDR_SPLIT(&ll->mac)
+			);
+			break;
+		}
+		default:
+			n += snprintf(
+				buf + n,
+				len - n,
+				" / Option type=%hhu len=%u(%u)",
+				opt->type,
+				opt->len * 8,
+				opt->len
+			);
+			break;
+		}
+		*offset += opt->len * 8;
+		payload_len -= opt->len * 8;
+		opt = rte_pktmbuf_mtod_offset(m, const struct icmp6_opt *, *offset);
+	}
+
+	return n;
 }

modules/infra/datapath/gr_icmp6.h
@@ -1,0 +1,131 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Robin Jarry
+
+#ifndef _GR_DATAPATH_ICMP6
+#define _GR_DATAPATH_ICMP6
+
+#include <gr_macro.h>
+
+#include <rte_byteorder.h>
+#include <rte_ether.h>
+#include <rte_ip6.h>
+
+#include <endian.h>
+#include <stdint.h>
+
+// ICMP6 packet types
+typedef enum {
+	ICMP6_TYPE_ECHO_REQUEST = UINT8_C(128),
+	ICMP6_TYPE_ECHO_REPLY = UINT8_C(129),
+	ICMP6_TYPE_ROUTER_SOLICIT = UINT8_C(133),
+	ICMP6_TYPE_ROUTER_ADVERT = UINT8_C(134),
+	ICMP6_TYPE_NEIGH_SOLICIT = UINT8_C(135),
+	ICMP6_TYPE_NEIGH_ADVERT = UINT8_C(136),
+	_ICMP6_TYPE_MAX = UINT8_C(0xff),
+} __rte_packed icmp6_type_t;
+
+// ICMP6 preamble
+struct icmp6 {
+	icmp6_type_t type;
+	uint8_t code;
+	rte_be16_t cksum;
+} __rte_packed;
+
+// ICMP6_TYPE_ECHO_REQUEST
+struct icmp6_echo_request {
+	rte_be16_t ident;
+	rte_be16_t seqnum;
+} __rte_packed;
+
+// ICMP6_TYPE_ECHO_REPLY
+struct icmp6_echo_reply {
+	rte_be16_t ident;
+	rte_be16_t seqnum;
+} __rte_packed;
+
+// ICMP6_TYPE_ROUTER_SOLICIT
+struct icmp6_router_solicit {
+	uint32_t __reserved;
+} __rte_packed;
+
+// ICMP6_TYPE_ROUTER_ADVERT
+struct icmp6_router_advert {
+	uint8_t cur_hoplim;
+	uint8_t flags;
+#if BYTE_ORDER == BIG_ENDIAN
+	uint8_t managed_addr : 1, other_config : 1, __unused_flags : 6;
+#else
+	uint8_t __unused_flags : 6, managed_addr : 1, other_config : 1;
+#endif
+	rte_be16_t lifetime;
+	rte_be32_t reachable_time;
+	rte_be32_t retrans_timer;
+} __rte_packed;
+
+// ICMP6_TYPE_NEIGH_SOLICIT
+struct icmp6_neigh_solicit {
+	uint32_t __reserved;
+	struct rte_ipv6_addr target;
+} __rte_packed;
+
+// ICMP6_TYPE_NEIGH_ADVERT
+struct icmp6_neigh_advert {
+#if BYTE_ORDER == BIG_ENDIAN
+	uint8_t router : 1, solicited : 1, override : 1, __unused_flags : 5;
+#else
+	uint8_t __unused_flags : 5, override : 1, solicited : 1, router : 1;
+#endif
+	uint8_t __reserved;
+	uint16_t __reserved2;
+	struct rte_ipv6_addr target;
+} __rte_packed;
+
+// ICMP6 options
+
+// option types
+typedef enum {
+	ICMP6_OPT_SRC_LLADDR = UINT8_C(1),
+	ICMP6_OPT_TARGET_LLADDR = UINT8_C(2),
+	ICMP6_OPT_PREFIX = UINT8_C(3),
+	ICMP6_OPT_REDIRECT = UINT8_C(4),
+	ICMP6_OPT_MTU = UINT8_C(5),
+	_ICMP6_OPT_MAX = UINT8_C(0xff),
+} __rte_packed icmp6_opt_t;
+
+struct icmp6_opt {
+	icmp6_opt_t type;
+	uint8_t len;
+} __rte_packed;
+
+// size of an option payload in units of 8 bytes
+// (add 7 and truncate down to the next multiple of 8)
+#define ICMP6_OPT_LEN(len) ((((len) + 7) & UINT8_C(0xf8)) / 8)
+
+// ICMP6_OPT_SRC_LLADDR | ICMP6_OPT_TARGET_LLADDR
+struct icmp6_opt_lladdr {
+	struct rte_ether_addr mac;
+} __rte_aligned(2) __rte_packed;
+
+static inline bool
+icmp6_get_opt(const struct icmp6_opt *opt, size_t ip6_len, uint8_t type, void *value) {
+	while (ip6_len >= 8 && opt != NULL) {
+		if (opt->type != type)
+			goto next;
+
+		switch (opt->type) {
+		case ICMP6_OPT_TARGET_LLADDR:
+		case ICMP6_OPT_SRC_LLADDR:
+			struct icmp6_opt_lladdr *ll = PAYLOAD(opt);
+			rte_ether_addr_copy(&ll->mac, (struct rte_ether_addr *)value);
+			return true;
+		default:
+			break;
+		}
+next:
+		ip6_len -= opt->len * 8;
+		opt = (struct icmp6_opt *)((char *)opt + (opt->len * 8));
+	}
+	return false;
+}
+
+#endif

commit cf5ff10759333d678226c6f6b1f677fed463f7f8

ip6: account for extension headers in payload length
As stated in RFC 8200, section 3, the payload length description is:

  Length of the IPv6 payload, i.e., the rest of the packet following
  this IPv6 header, in octets. (Note that any extension headers (see
  Section 4) present are considered part of the payload, i.e., included
  in the length count.)

Account for extension headers (if any) before processing the payload
data. The same issue was present in both the data path and the trace
function.

Link: https://datatracker.ietf.org/doc/html/rfc8200#section-3
Fixes: cf84f676455d ("trace: add ipv6 support")
Fixes: 315e4e54bda9 ("ip6: add basic icmp6 echo and reply support")
Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/infra/datapath/trace.c
@@ -152,6 +152,7 @@ ipv4:
 					ext_size
 				);
 			offset += ext_size;
+			payload_len -= ext_size;
 			proto = next_proto;
 		};
 

modules/ip6/datapath/ip6_local.c
@@ -66,6 +66,7 @@ static uint16_t ip6_input_local_process(
 			if (next_proto < 0)
 				break;
 			rte_pktmbuf_adj(m, ext_size);
+			d->len -= ext_size;
 			d->proto = next_proto;
 		};
 