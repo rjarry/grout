rjarry/grout

commit 4b9e28692fa78bb2f7b9ed8d4a17ded7e34ab801

ip: clean resources on interface deletion
On interface deletion, delete associated routes. specify module
destructor priorities to ensure resources are not freed when accessing
them.

Signed-off-by: Christophe Fontaine <cfontain@redhat.com>

modules/ip/control/route.c
@@ -301,6 +301,64 @@ static void route4_fini(struct event_base *) {
 	vrf_fibs = NULL;
 }
 
+void ip4_route_cleanup(uint16_t vrf_id, struct nexthop *nh) {
+	struct rte_rib_node *rn = NULL;
+	struct rte_rib *rib;
+	ip4_addr_t local_ip;
+	uint8_t prefixlen;
+	uint64_t nh_idx;
+	ip4_addr_t ip;
+	uint32_t mask;
+
+	local_ip = nh->ip;
+	mask = rte_be_to_cpu_32(~(0xffffffff >> nh->prefixlen));
+
+	rib = rte_fib_get_rib(get_fib(vrf_id));
+	while ((rn = rte_rib_get_nxt(rib, 0, 0, rn, RTE_RIB_GET_NXT_ALL)) != NULL) {
+		rte_rib_get_nh(rn, &nh_idx);
+		nh = ip4_nexthop_get(nh_idx);
+
+		if (nh && (nh->ip & mask) == (local_ip & mask)) {
+			rte_rib_get_ip(rn, &ip);
+			rte_rib_get_depth(rn, &prefixlen);
+			ip = rte_be_to_cpu_32(ip);
+
+			LOG(DEBUG,
+			    "%s ip4_route_delete " IP4_ADDR_FMT "/%d via " IP4_ADDR_FMT,
+			    __func__,
+			    IP4_ADDR_SPLIT(&ip),
+			    prefixlen,
+			    IP4_ADDR_SPLIT(&nh->ip));
+
+			ip4_route_delete(vrf_id, ip, prefixlen);
+			ip4_route_delete(vrf_id, ip, 32);
+		}
+	}
+
+	if ((rn = rte_rib_lookup_exact(rib, 0, 0)) != NULL || true) {
+		rte_rib_get_nh(rn, &nh_idx);
+		nh = ip4_nexthop_get(nh_idx);
+
+		if (nh && (nh->ip & mask) == (local_ip & mask)) {
+			rte_rib_get_ip(rn, &ip);
+			rte_rib_get_depth(rn, &prefixlen);
+			ip = rte_be_to_cpu_32(ip);
+
+			LOG(DEBUG,
+			    "%s ip4_route_delete " IP4_ADDR_FMT "/%d via " IP4_ADDR_FMT,
+			    __func__,
+			    IP4_ADDR_SPLIT(&ip),
+			    prefixlen,
+			    IP4_ADDR_SPLIT(&nh->ip));
+
+			ip4_route_delete(vrf_id, nh->ip, nh->prefixlen);
+			ip4_route_delete(vrf_id, nh->ip, 32);
+		}
+	}
+
+	ip4_route_delete(vrf_id, local_ip, 32);
+}
+
 static struct gr_api_handler route4_add_handler = {
 	.name = "ipv4 route add",
 	.request_type = GR_IP4_ROUTE_ADD,

modules/ip/control/address.c
@@ -142,6 +142,19 @@ static struct api_out addr_list(const void *request, void **response) {
 	return api_out(0, len);
 }
 
+static void iface_event_handler(iface_event_t event, struct iface *iface) {
+	struct nexthop *nh;
+
+	if (event != IFACE_EVENT_PRE_REMOVE)
+		return;
+
+	nh = ip4_addr_get(iface->id);
+	if (nh)
+		ip4_route_cleanup(iface->vrf_id, nh);
+
+	addrs[iface->id] = NULL;
+}
+
 static void addr_init(struct event_base *) {
 	addrs = rte_calloc(__func__, MAX_IFACES, sizeof(struct nexthop *), RTE_CACHE_LINE_SIZE);
 	if (addrs == NULL)
@@ -172,6 +185,11 @@ static struct gr_module addr_module = {
 	.name = "ipv4 address",
 	.init = addr_init,
 	.fini = addr_fini,
+	.fini_prio = 2000,
+};
+
+static struct iface_event_handler iface_event_address_handler = {
+	.callback = iface_event_handler,
 };
 
 RTE_INIT(address_constructor) {
@@ -179,4 +197,5 @@ RTE_INIT(address_constructor) {
 	gr_register_api_handler(&addr_del_handler);
 	gr_register_api_handler(&addr_list_handler);
 	gr_register_module(&addr_module);
+	iface_event_register_handler(&iface_event_address_handler);
 }

modules/ip/control/gr_ip4_control.h
@@ -58,7 +58,7 @@ int ip4_route_insert(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen, uint32_t
 int ip4_route_delete(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen);
 struct nexthop *ip4_route_lookup(uint16_t vrf_id, ip4_addr_t ip);
 struct nexthop *ip4_route_lookup_exact(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen);
+void ip4_route_cleanup(uint16_t vrf_id, struct nexthop *nh);
 
 struct nexthop *ip4_addr_get(uint16_t iface_id);
-
 #endif

modules/ip/control/nexthop.c
@@ -323,6 +323,7 @@ static struct gr_module nh4_module = {
 	.name = "ipv4 nexthop",
 	.init = nh4_init,
 	.fini = nh4_fini,
+	.fini_prio = 20000,
 };
 
 RTE_INIT(control_ip_init) {

commit 122216aa10fb8e5ff51f4ee90b7e79e4ec46711a

ip: do not delete route entries while iterating
Deleting routes while iterating over an rte_rib tree can lead to
use-after-free and/or undefined behavior. Even if the rib nodes are put
back to a mempool and we are not accessing "garbage" memory, the rib
tree may get disconnected and we may follow nonsensical pointers.

Change the functions that do that to collect all route entries that need
to be deleted into a vector and delete them after the iteration is
complete.

Fixes: 4b9e28692fa7 ("ip: clean resources on interface deletion")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/ip/control/route.c
@@ -286,6 +286,7 @@ int rib4_delete(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen, gr_nh_type_t 
 	if (fib == NULL)
 		return -errno;
 
+	LOG(DEBUG, "VRF %u: " IP4_F "/%hhu", vrf_id, &ip, prefixlen);
 	rib = rte_fib_get_rib(fib);
 	rn = rte_rib_lookup_exact(rib, rte_be_to_cpu_32(ip), prefixlen);
 	if (rn == NULL)
@@ -513,7 +514,19 @@ static struct api_out route4_list(const void *request, struct api_ctx *ctx) {
 	return api_out(-ret, 0, NULL);
 }
 
-static int rib4_cleanup_nh(
+struct rib4_cleanup_entry {
+	uint16_t vrf_id;
+	ip4_addr_t ip;
+	uint8_t depth;
+	gr_nh_type_t type;
+};
+
+struct rib4_cleanup_ctx {
+	const struct nexthop *nh;
+	gr_vec struct rib4_cleanup_entry *entries;
+};
+
+static int rib4_cleanup_cb(
 	uint16_t vrf_id,
 	ip4_addr_t ip,
 	uint8_t depth,
@@ -521,22 +534,34 @@ static int rib4_cleanup_nh(
 	const struct nexthop *nh,
 	void *priv
 ) {
-	const struct nexthop *hop = priv;
-	if (nh == hop) {
-		LOG(DEBUG, "delete " IP4_F "/%hhu via %u", &ip, depth, nh->nh_id);
-		rib4_delete(vrf_id, ip, depth, nh->type);
+	struct rib4_cleanup_ctx *ctx = priv;
+	if (ctx->nh == NULL || nh == ctx->nh) {
+		struct rib4_cleanup_entry entry = {
+			.vrf_id = vrf_id,
+			.ip = ip,
+			.depth = depth,
+			.type = nh->type,
+		};
+		gr_vec_add(ctx->entries, entry);
 	}
 	return 0;
 }
 
 void rib4_cleanup(struct nexthop *nh) {
+	struct rib4_cleanup_ctx ctx = {
+		.nh = nh,
+		.entries = NULL,
+	};
 	struct rib4_iterator iter = {
 		.max_count = 0,
 		.skip_internal = false,
-		.cb = rib4_cleanup_nh,
-		.priv = nh,
+		.cb = rib4_cleanup_cb,
+		.priv = &ctx,
 	};
 	rib4_iter(GR_VRF_ID_UNDEF, &iter);
+	gr_vec_foreach_ref (struct rib4_cleanup_entry *r, ctx.entries)
+		rib4_delete(r->vrf_id, r->ip, r->depth, r->type);
+	gr_vec_free(ctx.entries);
 }
 
 METRIC_GAUGE(m_routes, "rib4_routes", "Number of IPv4 routes by origin.");
@@ -752,47 +777,28 @@ static struct gr_module route4_module = {
 	.depends_on = "nexthop",
 };
 
-static int route4_vrf_rm(
-	uint16_t vrf_id,
-	ip4_addr_t ip,
-	uint8_t depth,
-	gr_nh_origin_t origin,
-	const struct nexthop *nh,
-	void *priv
-) {
-	const struct iface *vrf = priv;
-
-	LOG(DEBUG, "VRF %s(%u): " IP4_F "/%hhu", vrf->name, vrf->id, &ip, depth);
-
-	if (origin != GR_NH_ORIGIN_INTERNAL) {
-		gr_event_push(
-			GR_EVENT_IP_ROUTE_DEL,
-			&(const struct route4_event) {
-				.dest = {ip, depth},
-				.vrf_id = vrf_id,
-				.origin = origin,
-				.nh = nh,
-			}
-		);
-	}
-	nexthop_decref((void *)nh);
-
-	return 0;
-}
-
 static void fib4_fini(struct iface *vrf) {
 	struct rte_fib *fib = iface_info_vrf(vrf)->fib4;
 
-	iface_info_vrf(vrf)->fib4 = NULL;
 	if (fib != NULL) {
 		LOG(INFO, "destroying IPv4 FIB for VRF %s(%u)", vrf->name, vrf->id);
+		struct rib4_cleanup_ctx ctx = {
+			.nh = NULL,
+			.entries = NULL,
+		};
 		struct rib4_iterator iter = {
 			.max_count = 0,
 			.skip_internal = false,
-			.cb = route4_vrf_rm,
-			.priv = vrf,
+			.cb = rib4_cleanup_cb,
+			.priv = &ctx,
 		};
 		rib4_iter_vrf(rte_fib_get_rib(fib), vrf->id, &iter);
+
+		gr_vec_foreach_ref (struct rib4_cleanup_entry *r, ctx.entries)
+			rib4_delete(r->vrf_id, r->ip, r->depth, r->type);
+		gr_vec_free(ctx.entries);
+
+		iface_info_vrf(vrf)->fib4 = NULL;
 		rte_fib_free(fib);
 	}
 	memset(route_counts[vrf->id], 0, sizeof(route_counts[vrf->id]));