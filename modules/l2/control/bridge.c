rjarry/grout

commit fdd23aaad01f9fa0fadc6525686bddbe23f978fa

l2: add bridge interface type
Introduce a new l2 module with a bridge interface type that allows
grouping multiple member interfaces (ports, VLANs, bonds) into a single
L2 broadcast domain.

The bridge maintains a list of members and supports configurable MAC
learning, BUM traffic flooding, per-bridge ageing timer and a custom
MAC address. Members are switched to GR_IFACE_MODE_BRIDGE when attached
and restored to the default VRF when the bridge is destroyed.

FDB management and datapath nodes for actual packet forwarding will
follow in subsequent commits.

Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

modules/l2/control/bridge.c
@@ -1,0 +1,153 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2026 Robin Jarry
+
+#include <gr_event.h>
+#include <gr_l2_control.h>
+#include <gr_rcu.h>
+#include <gr_vrf.h>
+
+#include <rte_ether.h>
+#include <rte_hash.h>
+
+static int bridge_reconfig(
+	struct iface *iface,
+	uint64_t set_attrs,
+	const struct gr_iface *,
+	const void *api_info
+) {
+	struct iface_info_bridge *cur = iface_info_bridge(iface);
+	const struct gr_iface_info_bridge *next = api_info;
+
+	if (set_attrs & GR_BRIDGE_SET_MAC)
+		iface_set_eth_addr(iface, &next->mac);
+	if (set_attrs & GR_BRIDGE_SET_FLAGS)
+		cur->flags = next->flags;
+	if (set_attrs & GR_BRIDGE_SET_AGEING_TIME)
+		cur->ageing_time = next->ageing_time ?: GR_BRIDGE_DEFAULT_AGEING;
+
+	return 0;
+}
+
+static int bridge_attach_member(struct iface *bridge, struct iface *member) {
+	struct iface_info_bridge *br = iface_info_bridge(bridge);
+
+	switch (member->type) {
+	case GR_IFACE_TYPE_PORT:
+	case GR_IFACE_TYPE_VLAN:
+	case GR_IFACE_TYPE_BOND:
+		break;
+	default:
+		return errno_set(EMEDIUMTYPE);
+	}
+
+	for (unsigned i = 0; i < br->n_members; i++) {
+		if (br->members[i] == member)
+			return 0; // already a member
+	}
+
+	if (br->n_members == ARRAY_DIM(br->members))
+		return errno_set(EUSERS);
+
+	br->members[br->n_members++] = member;
+	member->domain_id = bridge->id;
+	member->vrf_id = GR_VRF_ID_UNDEF;
+	member->mode = GR_IFACE_MODE_BRIDGE;
+
+	return 0;
+}
+
+static int bridge_detach_member(struct iface *bridge, struct iface *member) {
+	struct iface_info_bridge *br = iface_info_bridge(bridge);
+
+	for (unsigned i = 0; i < br->n_members; i++) {
+		if (br->members[i] == member) {
+			unsigned last = br->n_members - 1;
+			if (i < last)
+				br->members[i] = br->members[last];
+			br->n_members--;
+			member->domain_id = GR_IFACE_ID_UNDEF;
+			member->mode = GR_IFACE_MODE_VRF;
+			break;
+		}
+	}
+
+	return 0;
+}
+
+static int bridge_fini(struct iface *iface) {
+	struct iface_info_bridge *bridge = iface_info_bridge(iface);
+
+	for (unsigned i = 0; i < bridge->n_members; i++) {
+		struct iface *member = bridge->members[i];
+		member->vrf_id = vrf_default_get_or_create();
+		if (member->vrf_id != GR_VRF_ID_UNDEF)
+			vrf_incref(member->vrf_id);
+		member->domain_id = GR_IFACE_ID_UNDEF;
+		member->mode = GR_IFACE_MODE_VRF;
+		gr_event_push(GR_EVENT_IFACE_POST_RECONFIG, member);
+	}
+
+	return 0;
+}
+
+static int bridge_init(struct iface *iface, const void *api_info) {
+	int ret;
+
+	iface->domain_id = iface->id; // for convenience, bridges are in their own domain
+
+	ret = bridge_reconfig(iface, IFACE_SET_ALL, NULL, api_info);
+	if (ret < 0) {
+		bridge_fini(iface);
+		errno = -ret;
+	}
+
+	return ret;
+}
+
+static int bridge_get_eth_addr(const struct iface *iface, struct rte_ether_addr *mac) {
+	const struct iface_info_bridge *bridge = iface_info_bridge(iface);
+	*mac = bridge->mac;
+	return 0;
+}
+
+static int bridge_set_eth_addr(struct iface *iface, const struct rte_ether_addr *mac) {
+	struct iface_info_bridge *bridge = iface_info_bridge(iface);
+
+	if (rte_is_zero_ether_addr(mac)) {
+		rte_eth_random_addr(bridge->mac.addr_bytes);
+	} else {
+		bridge->mac = *mac;
+	}
+
+	return 0;
+}
+
+static void bridge_to_api(void *info, const struct iface *iface) {
+	const struct iface_info_bridge *bridge = iface_info_bridge(iface);
+	struct gr_iface_info_bridge *api = info;
+
+	api->ageing_time = bridge->ageing_time;
+	api->flags = bridge->flags;
+	api->mac = bridge->mac;
+	api->n_members = bridge->n_members;
+	for (unsigned i = 0; i < bridge->n_members; i++)
+		api->members[i] = bridge->members[i]->id;
+}
+
+static struct iface_type iface_type_bridge = {
+	.id = GR_IFACE_TYPE_BRIDGE,
+	.pub_size = sizeof(struct gr_iface_info_bridge),
+	.priv_size = sizeof(struct iface_info_bridge),
+	.init = bridge_init,
+	.reconfig = bridge_reconfig,
+	.fini = bridge_fini,
+	.attach_domain = bridge_attach_member,
+	.detach_domain = bridge_detach_member,
+	.get_eth_addr = bridge_get_eth_addr,
+	.set_eth_addr = bridge_set_eth_addr,
+	.to_api = bridge_to_api,
+};
+
+RTE_INIT(bridge_constructor) {
+	iface_type_register(&iface_type_bridge);
+}

modules/l2/cli/bridge.c
@@ -1,0 +1,210 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2026 Robin Jarry
+
+#include <gr_api.h>
+#include <gr_cli.h>
+#include <gr_cli_iface.h>
+#include <gr_l2.h>
+#include <gr_net_types.h>
+#include <gr_table.h>
+
+#include <ecoli.h>
+
+#include <errno.h>
+#include <sys/queue.h>
+
+static void bridge_show(struct gr_api_client *c, const struct gr_iface *iface) {
+	const struct gr_iface_info_bridge *bridge = PAYLOAD(iface);
+
+	printf("flags: %sflood %slearn\n",
+	       (bridge->flags & GR_BRIDGE_F_NO_FLOOD) ? "no_" : "",
+	       (bridge->flags & GR_BRIDGE_F_NO_LEARN) ? "no_" : "");
+
+	printf("ageing_time: %u seconds\n", bridge->ageing_time);
+	printf("mac: " ETH_F "\n", &bridge->mac);
+	printf("members:\n");
+
+	for (uint8_t i = 0; i < bridge->n_members; i++) {
+		struct gr_iface *member = iface_from_id(c, bridge->members[i]);
+		if (member != NULL)
+			printf("- %s\n", member->name);
+		free(member);
+	}
+}
+
+static void
+bridge_list_info(struct gr_api_client *, const struct gr_iface *iface, char *buf, size_t len) {
+	const struct gr_iface_info_bridge *bridge = PAYLOAD(iface);
+	snprintf(
+		buf,
+		len,
+		"members=%u %sflood %slearn",
+		bridge->n_members,
+		(bridge->flags & GR_BRIDGE_F_NO_FLOOD) ? "no_" : "",
+		(bridge->flags & GR_BRIDGE_F_NO_LEARN) ? "no_" : ""
+	);
+}
+
+static struct cli_iface_type bridge_type = {
+	.type_id = GR_IFACE_TYPE_BRIDGE,
+	.show = bridge_show,
+	.list_info = bridge_list_info,
+};
+
+static uint64_t parse_bridge_args(
+	struct gr_api_client *c,
+	const struct ec_pnode *p,
+	struct gr_iface *iface,
+	bool update
+) {
+	struct gr_iface_info_bridge *bridge = PAYLOAD(iface);
+	uint64_t set_attrs;
+
+	set_attrs = parse_iface_args(c, p, iface, sizeof(*bridge), update);
+
+	if (arg_str(p, "flood")) {
+		bridge->flags &= ~GR_BRIDGE_F_NO_FLOOD;
+		set_attrs |= GR_BRIDGE_SET_FLAGS;
+	} else if (arg_str(p, "no_flood")) {
+		bridge->flags |= GR_BRIDGE_F_NO_FLOOD;
+		set_attrs |= GR_BRIDGE_SET_FLAGS;
+	}
+	if (arg_str(p, "learn")) {
+		bridge->flags &= ~GR_BRIDGE_F_NO_LEARN;
+		set_attrs |= GR_BRIDGE_SET_FLAGS;
+	} else if (arg_str(p, "no_learn")) {
+		bridge->flags |= GR_BRIDGE_F_NO_LEARN;
+		set_attrs |= GR_BRIDGE_SET_FLAGS;
+	}
+
+	if (arg_u16(p, "AGE", &bridge->ageing_time) == 0)
+		set_attrs |= GR_BRIDGE_SET_AGEING_TIME;
+	else if (errno != ENOENT)
+		return 0;
+
+	if (arg_eth_addr(p, "MAC", &bridge->mac) == 0)
+		set_attrs |= GR_BRIDGE_SET_MAC;
+	else if (errno != ENOENT)
+		return 0;
+
+	if (set_attrs == 0)
+		errno = EINVAL;
+
+	return set_attrs;
+}
+
+static cmd_status_t bridge_add(struct gr_api_client *c, const struct ec_pnode *p) {
+	const struct gr_infra_iface_add_resp *resp;
+	struct gr_infra_iface_add_req *req = NULL;
+	void *resp_ptr = NULL;
+	size_t len;
+
+	len = sizeof(*req) + sizeof(struct gr_iface_info_bridge);
+	if ((req = calloc(1, len)) == NULL)
+		goto err;
+
+	req->iface.type = GR_IFACE_TYPE_BRIDGE;
+	req->iface.flags = GR_IFACE_F_UP;
+
+	if (parse_bridge_args(c, p, &req->iface, false) == 0)
+		goto err;
+
+	if (gr_api_client_send_recv(c, GR_INFRA_IFACE_ADD, len, req, &resp_ptr) < 0)
+		goto err;
+
+	free(req);
+	resp = resp_ptr;
+	printf("Created interface %u\n", resp->iface_id);
+	free(resp_ptr);
+	return CMD_SUCCESS;
+err:
+	free(req);
+	return CMD_ERROR;
+}
+
+static cmd_status_t bridge_set(struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_infra_iface_set_req *req = NULL;
+	cmd_status_t ret = CMD_ERROR;
+	size_t len;
+
+	len = sizeof(*req) + sizeof(struct gr_iface_info_bridge);
+	if ((req = calloc(1, len)) == NULL)
+		goto out;
+
+	if ((req->set_attrs = parse_bridge_args(c, p, &req->iface, true)) == 0)
+		goto out;
+
+	if (gr_api_client_send_recv(c, GR_INFRA_IFACE_SET, len, req, NULL) < 0)
+		goto out;
+
+	ret = CMD_SUCCESS;
+out:
+	free(req);
+	return ret;
+}
+
+#define BRIDGE_ATTRS_CMD IFACE_ATTRS_CMD ",(ageing_time AGE),(mac MAC),FLOOD,LEARN"
+
+#define BRIDGE_ATTRS_ARGS                                                                          \
+	IFACE_ATTRS_ARGS,                                                                          \
+		with_help(                                                                         \
+			"Expiration time for learned MAC addresses.",                              \
+			ec_node_uint("AGE", 0, UINT16_MAX, 10)                                     \
+		),                                                                                 \
+		with_help("Bridge ethernet address.", ec_node_re("MAC", ETH_ADDR_RE)),             \
+		EC_NODE_OR(                                                                        \
+			"FLOOD",                                                                   \
+			with_help(                                                                 \
+				"Enable flooding of BUM traffic.", ec_node_str("flood", "flood")   \
+			),                                                                         \
+			with_help(                                                                 \
+				"Disable flooding of BUM traffic.",                                \
+				ec_node_str("no_flood", "no_flood")                                \
+			)                                                                          \
+		),                                                                                 \
+		EC_NODE_OR(                                                                        \
+			"LEARN",                                                                   \
+			with_help("Enable MAC learning.", ec_node_str("learn", "learn")),          \
+			with_help("Disable MAC learning.", ec_node_str("no_learn", "no_learn"))    \
+		)
+
+static int ctx_init(struct ec_node *root) {
+	int ret;
+
+	ret = CLI_COMMAND(
+		INTERFACE_ADD_CTX(root),
+		"bridge NAME [" BRIDGE_ATTRS_CMD "]",
+		bridge_add,
+		"Create a new bridge interface.",
+		with_help("Interface name.", ec_node("any", "NAME")),
+		BRIDGE_ATTRS_ARGS
+	);
+	if (ret < 0)
+		return ret;
+	ret = CLI_COMMAND(
+		INTERFACE_SET_CTX(root),
+		"bridge NAME (name NEW_NAME)," BRIDGE_ATTRS_CMD,
+		bridge_set,
+		"Modify bridge parameters.",
+		with_help(
+			"Interface name.",
+			ec_node_dyn("NAME", complete_iface_names, INT2PTR(GR_IFACE_TYPE_BRIDGE))
+		),
+		with_help("New interface name.", ec_node("any", "NEW_NAME")),
+		BRIDGE_ATTRS_ARGS
+	);
+	if (ret < 0)
+		return ret;
+
+	return 0;
+}
+
+static struct cli_context ctx = {
+	.name = "bridge",
+	.init = ctx_init,
+};
+
+static void __attribute__((constructor, used)) init(void) {
+	cli_context_register(&ctx);
+	register_iface_type(&bridge_type);
+}

modules/infra/api/gr_infra.h
@@ -21,6 +21,7 @@ typedef enum : uint8_t {
 	GR_IFACE_TYPE_VLAN,
 	GR_IFACE_TYPE_IPIP,
 	GR_IFACE_TYPE_BOND,
+	GR_IFACE_TYPE_BRIDGE,
 	GR_IFACE_TYPE_COUNT
 } gr_iface_type_t;
 
@@ -56,6 +57,7 @@ typedef enum : uint8_t {
 	GR_IFACE_MODE_VRF = 0,
 	GR_IFACE_MODE_XC,
 	GR_IFACE_MODE_BOND,
+	GR_IFACE_MODE_BRIDGE,
 	GR_IFACE_MODE_COUNT
 } gr_iface_mode_t;
 
@@ -433,8 +435,6 @@ struct gr_infra_cpu_affinity_set_req {
 // Helper function to convert iface type enum to string
 static inline const char *gr_iface_type_name(gr_iface_type_t type) {
 	switch (type) {
-	case GR_IFACE_TYPE_UNDEF:
-		return "undef";
 	case GR_IFACE_TYPE_VRF:
 		return "vrf";
 	case GR_IFACE_TYPE_PORT:
@@ -445,6 +445,9 @@ static inline const char *gr_iface_type_name(gr_iface_type_t type) {
 		return "ipip";
 	case GR_IFACE_TYPE_BOND:
 		return "bond";
+	case GR_IFACE_TYPE_BRIDGE:
+		return "bridge";
+	case GR_IFACE_TYPE_UNDEF:
 	case GR_IFACE_TYPE_COUNT:
 		break;
 	}
@@ -460,6 +463,8 @@ static inline const char *gr_iface_mode_name(gr_iface_mode_t mode) {
 		return "XC";
 	case GR_IFACE_MODE_BOND:
 		return "bond";
+	case GR_IFACE_MODE_BRIDGE:
+		return "bridge";
 	case GR_IFACE_MODE_COUNT:
 		break;
 	}

modules/infra/control/ctlplane.c
@@ -397,6 +397,7 @@ static void iface_event(uint32_t event, const void *obj) {
 	case GR_IFACE_TYPE_PORT:
 	case GR_IFACE_TYPE_VLAN:
 	case GR_IFACE_TYPE_BOND:
+	case GR_IFACE_TYPE_BRIDGE:
 		break;
 	default:
 		return;

modules/infra/control/iface.c
@@ -34,6 +34,7 @@ static bool iface_type_valid(gr_iface_type_t type) {
 	case GR_IFACE_TYPE_VLAN:
 	case GR_IFACE_TYPE_IPIP:
 	case GR_IFACE_TYPE_BOND:
+	case GR_IFACE_TYPE_BRIDGE:
 		return true;
 	case GR_IFACE_TYPE_UNDEF:
 	case GR_IFACE_TYPE_COUNT:

modules/l2/api/gr_l2.h
@@ -1,0 +1,42 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2026 Robin Jarry
+
+#pragma once
+
+#include <gr_api.h>
+#include <gr_bitops.h>
+#include <gr_macro.h>
+#include <gr_net_types.h>
+
+#include <stdint.h>
+
+#define GR_L2_MODULE 0xbabe
+
+// Bridge configuration flags.
+typedef enum : uint16_t {
+	GR_BRIDGE_F_NO_FLOOD = GR_BIT16(0),
+	GR_BRIDGE_F_NO_LEARN = GR_BIT16(1),
+} gr_bridge_flags_t;
+
+#define GR_BRIDGE_MAX_MEMBERS 64
+#define GR_BRIDGE_DEFAULT_AGEING 300
+
+// Bridge reconfiguration attribute flags.
+#define GR_BRIDGE_SET_AGEING_TIME GR_BIT64(32)
+#define GR_BRIDGE_SET_FLAGS GR_BIT64(33)
+#define GR_BRIDGE_SET_MAC GR_BIT64(34)
+
+struct __gr_iface_info_bridge_base {
+	uint16_t ageing_time; // Learned MAC ageing time in seconds (0 = default)
+	gr_bridge_flags_t flags;
+	struct rte_ether_addr mac; // Randomly generated if not set explicitly.
+	uint16_t n_members;
+};
+
+// Info structure for GR_IFACE_TYPE_BRIDGE interfaces.
+// Only port, VLAN and bond interfaces can be members.
+// Members are reassigned to the default VRF when the bridge is destroyed.
+struct gr_iface_info_bridge {
+	BASE(__gr_iface_info_bridge_base);
+	uint16_t members[GR_BRIDGE_MAX_MEMBERS]; // Interface IDs of bridge members.
+};

modules/l2/api/meson.build
@@ -1,0 +1,6 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2026 Robin Jarry
+
+api_headers += files('gr_l2.h')
+
+api_inc += include_directories('.')

modules/l2/cli/meson.build
@@ -1,0 +1,6 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2026 Robin Jarry
+
+cli_src += files(
+    'bridge.c',
+)

commit 1afe79c186310203723e7e6d2b5f01430c9ad675

l2: enable promiscuous mode on bridge member ports
When an interface becomes a bridge member, it must accept frames
destined to any MAC address so the bridge can forward them. Without
promiscuous mode, the NIC hardware filter drops frames whose
destination MAC does not match the port's own address or any explicitly
programmed filter entry. This prevents the bridge from forwarding
unicast traffic between ports.

Enable promiscuous mode on member ports when they are attached to
a bridge, and disable it when they are detached or when the bridge is
destroyed. Ignore EOPNOTSUPP for interface types like VXLAN that do not
need it (traffic arrives as IP/UDP destined to the port's own address).

Fixes: fdd23aaad01f ("l2: add bridge interface type")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/l2/control/bridge.c
@@ -49,6 +49,9 @@ static int bridge_attach_member(struct iface *bridge, struct iface *member) {
 	if (br->n_members == ARRAY_DIM(br->members))
 		return errno_set(EUSERS);
 
+	if (iface_set_promisc(member, true) < 0 && errno != EOPNOTSUPP)
+		return errno_log(errno, "iface_set_promisc(member)");
+
 	br->members[br->n_members++] = member;
 	member->domain_id = bridge->id;
 	member->vrf_id = GR_VRF_ID_UNDEF;
@@ -66,6 +69,7 @@ static int bridge_detach_member(struct iface *bridge, struct iface *member) {
 			if (i < last)
 				br->members[i] = br->members[last];
 			br->n_members--;
+			iface_set_promisc(member, false);
 			member->domain_id = GR_IFACE_ID_UNDEF;
 			member->mode = GR_IFACE_MODE_VRF;
 			fdb_purge_iface(member->id);
@@ -81,6 +85,7 @@ static int bridge_fini(struct iface *iface) {
 
 	for (unsigned i = 0; i < bridge->n_members; i++) {
 		struct iface *member = bridge->members[i];
+		iface_set_promisc(member, false);
 		member->vrf_id = vrf_default_get_or_create();
 		if (member->vrf_id != GR_VRF_ID_UNDEF)
 			vrf_incref(member->vrf_id);