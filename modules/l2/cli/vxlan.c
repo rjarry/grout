rjarry/grout

commit 8999062c56f2dde6382fd3404befbd51aa9c3432

vxlan: add support for IPv6 underlay
Allow VXLAN tunnels to use IPv6 addresses for the local VTEP and remote
flood VTEPs. Introduce struct l3_addr to hold either an IPv4 or IPv6
address with its address family, and use it throughout the VXLAN control
and datapath code.

On the TX side, compute the outer UDP checksum in software since IPv6
requires a non-zero UDP checksum (RFC 2460). On the RX side, tag mbufs
with the L3 packet type so vxlan_input can extract the source VTEP
address from either ip_local or ip6_local mbuf data.

Update the CLI and FRR plugin to accept IPv6 VTEP addresses. Use the
existing `#if CURRENT_FRR_VERSION >= MAKE_FRRVERSION(10, 6, 0)` macro
since only FRR-10.6+ supports IPv6 VXLAN VTEPs.

Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

modules/l2/cli/vxlan.c
@@ -63,7 +63,7 @@ static uint64_t parse_vxlan_args(
 		set_attrs |= GR_VXLAN_SET_VNI;
 	}
 
-	if (arg_ip4(p, "LOCAL", &vxlan->local) < 0) {
+	if (arg_ip_guess(p, "LOCAL", &vxlan->local.af, &vxlan->local.addr) < 0) {
 		if (errno != ENOENT)
 			return 0;
 	} else {
@@ -154,7 +154,7 @@ out:
 			"VXLAN Network Identifier (1-16777215).",                                  \
 			ec_node_uint("VNI", 1, 16777215, 10)                                       \
 		),                                                                                 \
-		with_help("Local VTEP IP address.", ec_node_re("LOCAL", IPV4_RE)),                 \
+		with_help("Local VTEP IP address.", ec_node_re("LOCAL", IP_ANY_RE)),               \
 		with_help(                                                                         \
 			"L3 routing domain name for encap addresses.",                             \
 			ec_node_dyn("ENCAP_VRF", complete_vrf_names, NULL)                         \

modules/l2/control/vxlan.c
@@ -3,12 +3,13 @@
 
 #include "event.h"
 #include "iface.h"
-#include "ip4.h"
 #include "ip4_datapath.h"
+#include "ip6_datapath.h"
 #include "l2.h"
 #include "l4.h"
 #include "log.h"
 #include "module.h"
+#include "nexthop.h"
 #include "rcu.h"
 #include "vrf.h"
 
@@ -117,21 +118,19 @@ static int iface_vxlan_reconfig(
 	}
 
 	if (set_attrs & (GR_VXLAN_SET_LOCAL | GR_VXLAN_SET_ENCAP_VRF)) {
-		ip4_addr_t local = (set_attrs & GR_VXLAN_SET_LOCAL) ? next->local : cur->local;
-		const struct nexthop *nh = rib4_lookup(cur->encap_vrf_id, local);
-		if (nh == NULL)
-			goto err;
-		if (nh->type != GR_NH_T_L3) {
-			errno = EPROTOTYPE;
+		struct l3_addr local = (set_attrs & GR_VXLAN_SET_LOCAL) ? next->local : cur->local;
+		const struct nexthop *nh = nexthop_lookup_l3(
+			local.af, cur->encap_vrf_id, cur->encap_vrf_id, &local.addr
+		);
+		if (nh == NULL) {
+			errno = EADDRNOTAVAIL;
 			goto err;
 		}
-
 		const struct nexthop_info_l3 *l3 = nexthop_info_l3(nh);
-		if (!(l3->flags & GR_NH_F_LOCAL)) {
+		if ((l3->flags & NH_LOCAL_ADDR_FLAGS) != NH_LOCAL_ADDR_FLAGS) {
 			errno = EPROTOTYPE;
 			goto err;
 		}
-
 		cur->local = local;
 		conf_done |= GR_VXLAN_SET_LOCAL;
 	}
@@ -143,13 +142,28 @@ static int iface_vxlan_reconfig(
 	}
 
 	// Update the datapath template from the current config.
-	cur->template.ip.version_ihl = IPV4_VERSION_IHL;
-	cur->template.ip.time_to_live = IPV4_DEFAULT_TTL;
-	cur->template.ip.next_proto_id = IPPROTO_UDP;
-	cur->template.ip.src_addr = cur->local;
-	cur->template.udp.dst_port = rte_cpu_to_be_16(cur->dst_port);
-	cur->template.vxlan.vx_flags = VXLAN_FLAGS_VNI;
-	cur->template.vxlan.vx_vni = vxlan_encode_vni(cur->vni);
+	switch (cur->local.af) {
+	case GR_AF_IP4:
+		cur->template.ipv4.ip.version_ihl = IPV4_VERSION_IHL;
+		cur->template.ipv4.ip.time_to_live = IPV4_DEFAULT_TTL;
+		cur->template.ipv4.ip.next_proto_id = IPPROTO_UDP;
+		cur->template.ipv4.ip.src_addr = cur->local.ipv4;
+		cur->template.ipv4.udp.dst_port = rte_cpu_to_be_16(cur->dst_port);
+		cur->template.ipv4.vxlan.vx_flags = VXLAN_FLAGS_VNI;
+		cur->template.ipv4.vxlan.vx_vni = vxlan_encode_vni(cur->vni);
+		break;
+	case GR_AF_IP6:
+		cur->template.ipv6.ip.vtc_flow = RTE_BE32(0x60000000);
+		cur->template.ipv6.ip.hop_limits = IP6_DEFAULT_HOP_LIMIT;
+		cur->template.ipv6.ip.proto = IPPROTO_UDP;
+		cur->template.ipv6.ip.src_addr = cur->local.ipv6;
+		cur->template.ipv6.udp.dst_port = rte_cpu_to_be_16(cur->dst_port);
+		cur->template.ipv6.vxlan.vx_flags = VXLAN_FLAGS_VNI;
+		cur->template.ipv6.vxlan.vx_vni = vxlan_encode_vni(cur->vni);
+		break;
+	case GR_AF_UNSPEC:
+		break;
+	}
 
 	if (conf_done & GR_VXLAN_SET_ENCAP_VRF)
 		vrf_decref(prev.encap_vrf_id);
@@ -284,8 +298,8 @@ static void vxlan_pre_remove_cb(uint32_t /*ev_type*/, const void *obj) {
 }
 
 static int vtep_flood_add(const struct gr_flood_entry *entry, bool exist_ok) {
+	struct l3_addr *vteps, *old_vteps;
 	struct iface_info_vxlan *vxlan;
-	ip4_addr_t *vteps, *old_vteps;
 	struct iface *iface;
 
 	iface = vxlan_get_iface(rte_cpu_to_be_32(entry->vtep.vni), entry->vrf_id);
@@ -295,7 +309,7 @@ static int vtep_flood_add(const struct gr_flood_entry *entry, bool exist_ok) {
 	vxlan = iface_info_vxlan(iface);
 
 	for (uint16_t i = 0; i < vxlan->n_flood_vteps; i++) {
-		if (vxlan->flood_vteps[i] == entry->vtep.addr) {
+		if (l3_addr_eq(&vxlan->flood_vteps[i], &entry->vtep.addr)) {
 			if (exist_ok)
 				return 0;
 			return errno_set(EEXIST);
@@ -336,7 +350,7 @@ static int vtep_flood_del(const struct gr_flood_entry *entry, bool missing_ok) {
 	vxlan = iface_info_vxlan(iface);
 
 	for (uint16_t i = 0; i < vxlan->n_flood_vteps; i++) {
-		if (vxlan->flood_vteps[i] == entry->vtep.addr) {
+		if (l3_addr_eq(&vxlan->flood_vteps[i], &entry->vtep.addr)) {
 			vxlan->flood_vteps[i] = vxlan->flood_vteps[vxlan->n_flood_vteps - 1];
 			vxlan->n_flood_vteps--;
 			event_push(GR_EVENT_FLOOD_DEL, entry);

api/gr_api.h
@@ -11,7 +11,7 @@
 #include <stdlib.h>
 
 // Must be bumped when making non-backward compatible changes in API headers
-#define GR_API_VERSION 1
+#define GR_API_VERSION 2
 
 // API request header.
 struct gr_api_request {

api/gr_net_types.h
@@ -13,6 +13,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #ifdef __GROUT_MAIN__
 #include <rte_ether.h>
@@ -172,3 +173,28 @@ out:
 
 #define IP_ANY_RE "^(" __IPV4_RE "|" __IPV6_RE ")$"
 #define IP_ANY_NET_RE "^(" __IPV4_RE __IPV4_PREFIX_RE "|" __IPV6_RE __IPV6_PREFIX_RE ")$"
+
+struct l3_addr {
+	addr_family_t af;
+	union {
+		struct {
+		} addr;
+		ip4_addr_t ipv4;
+		struct rte_ipv6_addr ipv6;
+	};
+};
+
+static inline bool l3_addr_eq(const struct l3_addr *a, const struct l3_addr *b) {
+	if (a->af != b->af)
+		return false;
+
+	switch (a->af) {
+	case GR_AF_IP4:
+		return a->ipv4 == b->ipv4;
+	case GR_AF_IP6:
+		return memcmp(&a->ipv6, &b->ipv6, sizeof(a->ipv6)) == 0;
+	default:
+		break;
+	}
+	return true;
+}

cli/cli.h
@@ -64,6 +64,7 @@ int arg_eth_addr(const struct ec_pnode *p, const char *id, struct rte_ether_addr
 int arg_ip(const struct ec_pnode *p, const char *id, void *addr, int af);
 int arg_ip4(const struct ec_pnode *p, const char *id, ip4_addr_t *addr);
 int arg_ip6(const struct ec_pnode *p, const char *id, struct rte_ipv6_addr *addr);
+int arg_ip_guess(const struct ec_pnode *p, const char *id, addr_family_t *af, void *addr);
 int arg_ip_net(const struct ec_pnode *p, const char *id, void *net, bool zero_mask, int af);
 int arg_ip4_net(const struct ec_pnode *p, const char *id, struct ip4_net *net, bool zero_mask);
 int arg_ip6_net(const struct ec_pnode *p, const char *id, struct ip6_net *net, bool zero_mask);

cli/ecoli.c
@@ -192,6 +192,17 @@ int arg_ip6(const struct ec_pnode *p, const char *id, struct rte_ipv6_addr *addr
 	return arg_ip(p, id, addr, AF_INET6);
 }
 
+int arg_ip_guess(const struct ec_pnode *p, const char *id, addr_family_t *af, void *addr) {
+	if (arg_ip4(p, id, addr) == 0) {
+		*af = GR_AF_IP4;
+	} else if (arg_ip6(p, id, addr) == 0) {
+		*af = GR_AF_IP6;
+	} else {
+		return -errno;
+	}
+	return 0;
+}
+
 int arg_ip_net(const struct ec_pnode *p, const char *id, void *net, bool zero_mask, int family) {
 	const char *str = arg_str(p, id);
 	if (str == NULL)

frr/if_grout.c
@@ -4,6 +4,7 @@
 #include "if_grout.h"
 #include "if_map.h"
 #include "log_grout.h"
+#include "zebra_dplane_grout.h"
 
 #include <gr_ip4.h>
 #include <gr_ip6.h>
@@ -11,6 +12,7 @@
 #include <gr_srv6.h>
 
 #include <lib/libfrr.h>
+#include <lib/version.h>
 #include <linux/if.h>
 #include <net/if.h>
 #include <zebra/interface.h>
@@ -95,6 +97,17 @@ void grout_link_change(struct gr_iface *gr_if, bool new, bool startup) {
 		break;
 	case GR_IFACE_TYPE_VXLAN:
 		gr_vxlan = (const struct gr_iface_info_vxlan *)&gr_if->info;
+#if CURRENT_FRR_VERSION < MAKE_FRRVERSION(10, 6, 0)
+		if (gr_vxlan->local.af != GR_AF_IP4) {
+			gr_log_err(
+				"iface %s: %s local vtep not supported in frr %s",
+				gr_if->name,
+				gr_af_name(gr_vxlan->local.af),
+				FRR_VER_SHORT
+			);
+			return;
+		}
+#endif
 		link_type = ZEBRA_LLT_ETHER;
 		zif_type = ZEBRA_IF_VXLAN;
 		mac = &gr_vxlan->mac;
@@ -179,10 +192,9 @@ void grout_link_change(struct gr_iface *gr_if, bool new, bool startup) {
 			vi.vni_info.vni.vni = gr_vxlan->vni;
 			vi.ifindex_link = ifindex_grout_to_frr(gr_vxlan->encap_vrf_id);
 #if CURRENT_FRR_VERSION >= MAKE_FRRVERSION(10, 6, 0)
-			vi.vtep_ip.ipa_type = IPADDR_V4;
-			vi.vtep_ip.ipaddr_v4.s_addr = gr_vxlan->local;
+			l3_addr_to_ipaddr(&vi.vtep_ip, &gr_vxlan->local);
 #else
-			vi.vtep_ip.s_addr = gr_vxlan->local;
+			vi.vtep_ip.s_addr = gr_vxlan->local.ipv4;
 #endif
 			dplane_ctx_set_ifp_vxlan_info(ctx, &vi);
 		}

commit 043901089d7c10030769bab4350646806a9bfdc2

vxlan: fix display of local vtep address
The local VTEP address can now be IPv6.

Fixes: 8999062c56f2 ("vxlan: add support for IPv6 underlay")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/l2/cli/vxlan.c
@@ -17,7 +17,7 @@ static void vxlan_show(struct gr_api_client *c, const struct gr_iface *iface, st
 	const struct gr_iface_info_vxlan *vxlan = (const struct gr_iface_info_vxlan *)iface->info;
 
 	gr_object_field(o, "vni", GR_DISP_INT, "%u", vxlan->vni);
-	gr_object_field(o, "local", 0, IP4_F, &vxlan->local);
+	gr_object_field(o, "local", 0, ADDR_F, ADDR_W(vxlan->local.af), &vxlan->local.addr);
 	gr_object_field(o, "encap_vrf", 0, "%s", iface_name_from_id(c, vxlan->encap_vrf_id));
 	gr_object_field(o, "dst_port", GR_DISP_INT, "%u", vxlan->dst_port);
 	gr_object_field(o, "mac", 0, ETH_F, &vxlan->mac);
@@ -30,9 +30,10 @@ vxlan_list_info(struct gr_api_client *c, const struct gr_iface *iface, char *buf
 	snprintf(
 		buf,
 		len,
-		"vni=%u local=" IP4_F " encap_vrf=%s",
+		"vni=%u local=" ADDR_F " encap_vrf=%s",
 		vxlan->vni,
-		&vxlan->local,
+		ADDR_W(vxlan->local.af),
+		&vxlan->local.addr,
 		iface_name_from_id(c, vxlan->encap_vrf_id)
 	);
 }